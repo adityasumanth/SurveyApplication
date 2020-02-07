4b366c1b29cd686550c4c26d73b4b00b7255a43f
PRADEEP SHABAD <shabadpradeep1@gmail.com>
2020-02-07 11:39:41 +0530

    Added admin components.

---
+++ Survey.Web/ClientApp/src/app/admin-home/admin-home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-house',
+  templateUrl: './admin-house.component.html',
+  styleUrls: ['./admin-house.component.css']
+})
+
+export class AdminHouseComponent {
+  public
+}
