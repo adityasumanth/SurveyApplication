0903b0e615999ba336a3f0fa2a64a22bb4911a98
PRADEEP SHABAD <shabadpradeep1@gmail.com>
2020-02-07 17:18:13 +0530

    Added questions

--- Survey.Web/ClientApp/src/app/create-survey/create-survey.component.ts
+++ Survey.Web/ClientApp/src/app/create-survey/create-survey.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-survey',
@@ -6,8 +7,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./create-survey.component.css']
 })
 
-export class CreateSurveyComponent {
-  constructor() {
+export class CreateSurveyComponent implements OnInit {
+  surveyNameForm: FormGroup;
+  questions: question[];
+  newForm = true;
+  loading = false;
+  surveyName: string = '';
+
+  constructor(private formBuilder: FormBuilder) {
+  }
+
+  ngOnInit() {
+    this.surveyNameForm = this.formBuilder.group({
+      title: ['', Validators.required]
+    });
+    for()
+    this.questions
+  }
+
+  get f() { return this.surveyNameForm.controls; }
+
+  onNameSubmit() {
+    this.newForm = false;
+    this.surveyName = this.f.title.value;
+  }
+
+  CreateForm() {
+    this.loading = true;
   }
 }
 
+class question implements OnInit {
+  surveyForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+  }
+
+  ngOnInit() {
+    this.surveyForm = this.formBuilder.group({
+      question: [''],
+      option: ['']
+    });
+  }
+}
