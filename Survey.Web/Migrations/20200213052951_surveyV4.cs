b955870d94635c622cdd3462b53ca7c1661914ea
PRADEEP SHABAD <shabadpradeep1@gmail.com>
2020-02-13 11:46:26 +0530

    migration changes.

---
+++ Survey.Web/Migrations/20200213052951_surveyV4.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Survey.Web.Migrations
+{
+    public partial class surveyV4 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
